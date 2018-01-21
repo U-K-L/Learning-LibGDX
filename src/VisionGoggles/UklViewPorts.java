02293434d6175003a7eeceb467d2ef2e338d8a18
UKL <WilliamsNoah1720@gmail.com>
2018-01-20 21:14:02 -0500

    Resizing graphics

---
+++ src/VisionGoggles/UklViewPorts.java
@@ -0,0 +1,15 @@
+package VisionGoggles;
+
+import com.badlogic.gdx.utils.ArrayMap;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by Noah on 1/20/2018.
+ *
+ * Purpose: Resizes the screen to fit different types of screens.
+ */
+
+public class UklViewPorts {
+    private ArrayMap<String, Viewport> views;
+
+}
